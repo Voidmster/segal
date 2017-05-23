28b3ccfb79c48443fba02c2e6eb11e4f8297bc30
Voidmster <voidmaster666@gmail.com>
2017-05-24 00:41:50 +0300

    add tester

--- src/ru/ifmo/ctddev/segal_task/Main.java
+++ src/ru/ifmo/ctddev/segal_task/Main.java
@@ -3,15 +3,23 @@ package ru.ifmo.ctddev.segal_task;
 public class Main {
 
     public static void main(String[] args) {
-        FastScanner f = new FastScanner(System.in);
-        int n = f.nextInt();
+//        FastScanner f = new FastScanner(System.in);
+//        int n = f.nextInt();
+//        double [][] a = new double[10][10];
+//        for (int i = 0; i < 10; i++) {
+//            for (int j = 0; j < 10; j++) {
+//                a[i][j] = f.nextDouble();
+//            }
+//        }
+//        makeTest(a, n);
+        makeTest(Utils.getIllConditionedMatrix(10), 10);
+        makeTest(Utils.getIllConditionedMatrix(10), 10);
+        makeTest(Utils.identityMatrix(10), 10);
+        makeTest(Utils.zeroMatrix(10), 10);
+    }
+
+    public static void makeTest(double[][] a, int n) {
         for (int k = 0; k < n; k++) {
-            double [][] a = new double[10][10];
-            for (int i = 0; i < 10; i++) {
-                for (int j = 0; j < 10; j++) {
-                    a[i][j] = f.nextDouble();
-                }
-            }
             System.out.print("Gauss answer to test " + k + ":");
             Gauss.solve(a);
             System.out.print("Jacobi answer to test " + k + ":");
