25b322b2d8bd7ed8f1a403bdaf76a0fdc6c79c6b
Vladislav Kiryukhin <kiryukhinv@gmail.com>
2017-05-24 00:35:12 +0300

    Add matrix generators

---
+++ src/Utils.java
@@ -0,0 +1,38 @@
+public class Utils {
+    public static double[][] getWellConditionedMatrix(int n) {
+        double[][] a = new double[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = Math.pow(-0.5, Math.abs(j - i)) / n;
+            }
+            a[i][i] = -1;
+        }
+        return a;
+    }
+
+    public static double[][] getIllConditionedMatrix(int n) {
+        double[][] a = new double[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = 1.0 / (i + j + 1);
+            }
+        }
+        return a;
+    }
+
+    public static double[][] identityMatrix(int n) {
+        double[][] a = new double[n][n];
+
+        for (int i = 0; i < n; i++) {
+            a[i][i] = 1.0;
+        }
+        return a;
+    }
+
+    public static double[][] zeroMatrix(int n) {
+        double[][] a = new double[n][n];
+        return a;
+    }
+}
